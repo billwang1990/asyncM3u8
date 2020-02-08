c9a729dac760d355dcbbba4f71282a205f501ffc
yaqing <billwang1990@gmail.com>
2020-02-08 12:59:44 +0800

    [Kingwong] Add example.js

--- example.js
+++ example.js
@@ -1,24 +1,7 @@
-### 
-### AsyncM3u8
-
-An async m3u8 downloader for Node.js, based on ffmpeg.
-
-
-
-### Usage
-
-```
 const AsymM3u8 = require('./aysncM3u8');
 
 (async function () {
+    // const asyncM3u8 = new AsymM3u8('https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa_video_180_250000.m3u8');
     const asyncM3u8 = new AsymM3u8('http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/3340_vod.m3u8');
     await asyncM3u8.start();
 })();
-
-```
-
-
-
-### Note:
-
-Only support the m3u8 url which contain media playlist.
