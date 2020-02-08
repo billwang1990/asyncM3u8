aa8553a6af8e8200af968f299c59276d2c98c276
yaqing <billwang1990@gmail.com>
2020-02-08 12:58:56 +0800

    [Kingwong] Fix dependency issue

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const AsymM3u8 = require('./aysncM3u8');
-
-(async function () {
-    const asyncM3u8 = new AsymM3u8('http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/3340_vod.m3u8');
-    await asyncM3u8.start();
-})();
+module.exports = require('./aysncM3u8');
